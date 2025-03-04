f7566446e33ac161a376f0d8927d5af6f688fb35
bustedchickn <86179078+bustedchickn@users.noreply.github.com>
2025-03-04 14:17:25 -0700

    Update wording.py

--- wording.py
+++ wording.py
@@ -1,12 +1,13 @@
 import customtkinter as ctk
 import random
+import openpyxl
 
 
-x = ["rachel","susanna","sarah","LJ","Talmage"]
-print(x[random.randint(0,len(x))])
 
 
-'''
+
+
+
 # Initialize the main window
 root = ctk.CTk()
 root.geometry("500x300")
@@ -15,14 +16,8 @@ root.geometry("500x300")
 frame = ctk.CTkFrame(root)
 frame.pack(pady=20, padx=20)
 
-# Create two text boxes
-text_box1 = ctk.CTkTextbox(frame, width=200, height=100)
-text_box1.pack(side="left", padx=10)
 
-text_box2 = ctk.CTkTextbox(frame, width=200, height=100)
-text_box2.pack(side="left", padx=10)
 
-text_box1.insert(0.0,"Hey this is the new text for textbox1!")
+
 
 root.mainloop()
-'''
