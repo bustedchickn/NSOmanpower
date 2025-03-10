a74a023962274139bb36b1a0569a382d0d2dd0cf
bustedchickn <86179078+bustedchickn@users.noreply.github.com>
2025-03-10 16:21:58 -0600

    Update gptTesting.py

--- gptTesting.py
+++ gptTesting.py
@@ -1,3 +1,15 @@
 import math
-# print(math.ceil(-104/9))
-print(((15)*(79)*78)%7)
+listm = []
+# def baseconversion(k,b,l):
+    
+#     baseconversion(k//b,b,l)
+#     if k<0:
+#         return l.append(k%b)
+    
+# baseconversion(8,2,listm)
+# print(listm)
+k = 347
+b = 6
+
+print([k//b**4%b,k//b**3%b,k//b**2%b,k//b%b,k%b])
+print(44**12%6)
